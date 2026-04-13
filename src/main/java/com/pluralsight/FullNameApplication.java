700dae2b137c6c776d89458e6f688a8928a7f2ab
RickelviC <Cruzrickelvi@gmail.com>
2026-04-13 13:47:58 -0400

    added a while loop to make sure we get a first name and last name

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        String firstName = "";
+        String lastName = "";
+
+        while (firstName.equals("")){
+            System.out.println("please enter your first name");
+            System.out.print("First name: ");
+            firstName = scanner.nextLine();
+
+            if(firstName.equals("")){
+                System.out.println("must have a first name");
+            }
+        }
+
+
+
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine();
+
+
+        while (lastName.equals("")){
+            System.out.println("please enter your Last name");
+            System.out.print("Last name: ");
+            lastName = scanner.nextLine();
+
+            if(lastName.equals("")){
+                System.out.println("must have a Last name");
+            }
+        }
+
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine();
+
+        String fullName = firstName + " " + middleName + " " + lastName + " " + suffix;
+
+
+        System.out.print("Full name: " + fullName);
+
+    }
 }
